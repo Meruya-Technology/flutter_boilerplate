a9c9d7a97a9c1732159d9fa56f428f6150010de6
Dwi Kurnianto Mulyadien <dwi.kurnianto@evermos>
2021-09-12 11:00:22 +0700

    Update facade

---
+++ lib/common/core/error_handler_impl.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_boilerplate/common/core/custom_error.dart';
+import 'package:flutter_boilerplate/common/core/error_handler.dart';
+
+class ErrorHandlerImpl implements ErrorHandler {
+  @override
+  formatError(error) {
+    throw UnimplementedError();
+  }
+}
